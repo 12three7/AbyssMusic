460166a91e65959b80c96c679ad177ee1a95400c
1237 <92003875+12three7@users.noreply.github.com>
2022-05-02 20:15:13 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
-//Vars.control.sound.ambientMusic.add(loadMusic(""));
+Vars.control.sound.ambientMusic.add(loadMusic("pembukaan"));
+Vars.control.sound.darkMusic.add(loadMusic("melebihi-batas"));
+Vars.control.sound.ambientMusic.add(loadMusic("peradaban-baru"));
+Vars.control.sound.darkMusic.add(loadMusic("masalah"));
+Vars.control.sound.darkMusic.add(loadMusic("penemuan-berharga"));
+Vars.control.sound.ambientMusic.add(loadMusic("tempat-aneh"));
+Vars.control.sound.darkMusic.add(loadMusic("jurang-kematian"));
+Vars.control.sound.ambientMusic.add(loadMusic("misi-selesai"));
+Vars.control.sound.ambientMusic.add(loadMusic("mengulang-kembali"));
+
